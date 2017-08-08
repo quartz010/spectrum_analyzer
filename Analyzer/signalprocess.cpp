9eb3afc6f789289e01e3ea982a02c050bf19d126
phantom <phantom@localhost.localdomain>
2017-08-08 19:32:46 +0800

    backup

---
+++ Analyzer/signalprocess.cpp
@@ -0,0 +1,6 @@
+#include "signalprocess.h"
+
+SignalProcess::SignalProcess(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ Analyzer/signalprocess.h
@@ -0,0 +1,16 @@
+#ifndef SIGNALPROCESS_H
+#define SIGNALPROCESS_H
+
+
+class SignalProcess : public QObject
+{
+	Q_OBJECT
+public:
+	explicit SignalProcess(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // SIGNALPROCESS_H
